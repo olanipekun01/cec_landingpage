52199c5c84bd513108ce8896de5e073301a5b96a
codemayor19 <codemayor19@gmail.com>
2024-10-17 21:58:16 -0700

    l

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const menuBar = document.getElementById('menuBar');
 const navBar = document.getElementById('navBar');
 const xRegular = document.getElementById('xRegular');
+const faqs = document.querySelectorAll(".faq");
 
 menuBar.addEventListener("click", function () {
     navBar.classList.toggle('active');
@@ -12,4 +13,10 @@ xRegular.addEventListener("click", function() {
     navBar.classList.toggle('active');
     xRegular.classList.toggle('active');
     menuBar.classList.toggle('active');
+})
+
+faqs.forEach(faq => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("active");
+    })
 })
