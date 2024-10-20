5d2fe5e4709d0adf116cf391c0399eb6cb9ac4bb
olanipekun01 <oloniluaeyitayo@gmail.com>
2024-10-20 08:56:46 -0700

    l

--- php/nclex.php
+++ php/nclex.php
@@ -11,11 +11,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validate inputs
     if (!empty($fName) && !empty($lName)  && !empty($email) && !empty($phone) && !empty($status) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
         // Email details
-        $to = "oloniluaeyitayo@gmail.com";  // Replace with your own email
+        $to = "cecforms@achievers.edu.ng, Learnenglishachievers@glocotrust.com";  // Replace with your own email
         $subject = "New NCLEX Form Submission";
         
         // Set a fixed "From" email address (e.g., your domain's email)
-        $from = "olonilua.eo@achievers.edu.ng";  // Replace with your domain's email
+        $from = "ict@achievers.edu.ng";  // Replace with your domain's email
         
         $headers = "From: " . $from . "\r\n";
         $headers .= "Reply-To: " . $email . "\r\n";  // The user's email for replying
--- php/send_internship.php
+++ php/send_internship.php
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validate inputs
     if (!empty($fName) && !empty($lName)  && !empty($email) && !empty($phone) && !empty($industry) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
         // Email details
-        $to = "oloniluaeyitayo@gmail.com";  // Replace with your own email
+        $to = "cecforms@achievers.edu.ng, , internshipachievers@glocotrust.com, employmenttrainingachievers@glocotrust.com";  // Replace with your own email
         $subject = "New Internship Form Submission";
         
         // Set a fixed "From" email address (e.g., your domain's email)
